463ae19b9888b9a90aa1c1885c0ec09b8785faa7
Arijeet10 <arijeetsocial@gmail.com>
2024-05-09 18:43:36 +0530

    ui changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
       <div className="py-[2vh] sm:py-[4vh] flex flex-col sm:flex-row sm:items-center justify-between gap-[2vh]">
         <div className="flex items-center justify-center sm:justify-start">
-          <img src="/mainlogo.svg" alt="hyggex logo" className="w-[18-vw] h-[4vh]  sm:w-[14vw] sm:h-[4vh] " />
+          <img src="/mainlogo.svg" alt="hyggex logo" className="w-[18-vw] h-[4vh]  sm:w-[14vw] sm:h-[4vh] md:h-[10vh] " />
         </div>
 
         <div className="text-[#3A3740] flex items-center justify-between sm:justify-center gap-[2vw]">
